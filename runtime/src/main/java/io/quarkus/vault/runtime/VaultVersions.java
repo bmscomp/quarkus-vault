9bff6c407e2e5edbbe9a3921914828d80976e142
Said BOUDJELDA <bmscomp@gmail.com>
2023-10-04 22:19:03 +0200

    update version of vault

--- runtime/src/main/java/io/quarkus/vault/runtime/VaultVersions.java
+++ runtime/src/main/java/io/quarkus/vault/runtime/VaultVersions.java
@@ -2,9 +2,9 @@ package io.quarkus.vault.runtime;
 
 public final class VaultVersions {
 
-    public static final String VAULT_TEST_VERSION = "1.10.0";
+    public static final String VAULT_TEST_VERSION = "1.15.0";
 
     private VaultVersions() {
-        
+
     }
 }
